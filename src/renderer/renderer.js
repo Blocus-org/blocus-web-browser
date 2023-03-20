270839b7f90179fced35ece39a2d5806cc065f35
Zqfd <zqfdrshj@protonmail.com>
2023-03-20 14:12:49 +0100

    Signed-off-by: Zqfd <zqfdrshj@protonmail.com>

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -12,42 +12,67 @@ var forwButton = document.createElement('button')
 var meta1 = document.getElementById("meta1-csp")
 var divUrl = document.createElement('div')
 var pos = ''
+var pageTitle = ''
+var refreshButton = document.createElement('button')
+var refreshButtonImg = document.createElement('img')
+var stopButton = document.createElement('button')
+var stopButtonImg = document.createElement('img')
 var urlBar = document.createElement('input')
-var urls = []
 var url = ''
-var webviews = []
 var webview = ''
 
-const tabGroup = document.querySelector("tab-group")
+const indicator = ""
+const tabGroup = document.querySelector('tab-group')
 const shadow = tabGroup.shadowRoot
 const views = shadow.querySelector('.views')
 
-
-//WIP
-/*tabGroup.on("tab-added", (tab, tabGroup) => {
-  if(tabGroup.getTabs() != null){  
-    tabGroup.eachTab(
-      function getTabURL() {
-        tab.once("webview-ready", (tab) => { 
-          webview = tab.webview
-        })
-      }
-    , this)
-    //urls = []
-    if(webview != '' && webview = shadow.querySelector('')){
-      if(log === true){
-        console.log('log: Webview created: ', webview)
-      }
-      webviews.push(webview)
-    }
-    if(log === true){
-      if(webviews.length > 0){
-        console.log('log: All webviews: ', webviews)
-      }
-      console.log('log: How many tabs: ',tabGroup.getTabs().length)
+function forward(){
+  if(webview.canGoForward()){
+    webview.goForward()
+    if (log === true) {
+      console.log("log: Forward")
     }
   }
-})*/
+}
+
+function back(){
+  if(webview.canGoBack())
+    webview.goBack()
+  if (log === true) {
+    console.log("log: Backward")
+  }
+}
+
+function refresh(){
+  webview.reload()
+  if (log === true) {
+    console.log("log: Refresh")
+  }
+}
+
+tabGroup.on("tab-added", (tab, tabGroup) => {
+  tab.on("webview-dom-ready", (tab) => {
+    webview = tab.webview
+    url = webview.getURL()
+    pageTitle = webview.getTitle()
+    tab.setTitle(pageTitle)
+    urlBar.setAttribute("value", url)
+    forwButton.setAttribute("onclick", "forward()")
+    backButton.setAttribute("onclick", "back()")
+    refreshButton.setAttribute("onclick", "refresh()")
+  })
+  if(log === true){
+      console.log('log: New tab', url)
+      console.log('log: Total tabs: ', tabGroup.getTabs())
+  }
+})
+
+tabGroup.on("tab-removed", (tab, tabGroup) => {
+  if(log === true){
+    console.log('log: Deleted tab')
+    console.log('log: Total tabs: ', tabGroup.getTabs())
+  }
+})
 
 meta1.setAttribute("class", "meta1-csp")
 meta1.setAttribute("http-equiv", "Content-Security-Policy")
@@ -55,7 +80,7 @@ meta1.setAttribute("content", "default-src 'self' ; style-src 'self' 'unsafe-inl
 
 backButton.setAttribute("id", "backbutton")
 backButton.setAttribute("class", "backbutton")
-//backButton.setAttribute("onclick", 'back()')
+
 buttonDiv.setAttribute("class", "buttondiv")
 divUrl.setAttribute("class", "divurl")
 imgBack.setAttribute("class", "imgback")
@@ -64,10 +89,15 @@ imgForw.setAttribute("class", "imgforw")
 imgForw.setAttribute("src", "../img/forwarrow.png")
 toolBar.setAttribute("class", "toolbar")
 formToolBar.setAttribute("class", "formtoolbar")
-formToolBar.setAttribute("method", "POST")
+formToolBar.setAttribute("method", "GET")
 forwButton.setAttribute("id", "forwbutton")
 forwButton.setAttribute("class", "forwbutton")
-//forwButton.setAttribute("onclick", 'forw()')
+refreshButton.setAttribute("class", "refreshbutton" )
+refreshButtonImg.setAttribute("class", "refreshbuttonimg")
+refreshButtonImg.setAttribute("src", "../img/refresh.png")
+stopButtonImg.setAttribute("class", "stopbuttonimg")
+stopButton.setAttribute("class", "stopbutton" )
+stopButtonImg.setAttribute("src", "../img/stop.png")
 urlBar.setAttribute("class", "toolbar-input")
 urlBar.setAttribute("placeholder", "  http://url.exemple.com  ")
 
@@ -77,17 +107,21 @@ divUrl.appendChild(formToolBar)
 toolBar.appendChild(divUrl)
 buttonDiv.appendChild(backButton)
 buttonDiv.appendChild(forwButton)
-//backButton.appendChild(imgBack)
-//forwButton.appendChild(imgForw)
+
+refreshButton.appendChild(refreshButtonImg)
+stopButton.appendChild(stopButtonImg)
+buttonDiv.appendChild(refreshButton)
+
+backButton.appendChild(imgBack)
+forwButton.appendChild(imgForw)
 formToolBar.appendChild(urlBar)
+
 
 views.prepend(toolBar)
 
-//observer.observe(views, {subtree: true, childList: true})
-
 tabGroup.setDefaultTab({
-  title: "New Tab",
+  title: pageTitle,
   src: "https://google.com",
   active: true
 })
-
+tabGroup.addTab();
